ed74a12dff603cc180ddc7d712c7369fa5ffe903
GeekchanskiY <astrovskiy.04@gmail.com>
2024-02-23 20:09:06 +0400

    test widgets

--- main.go
+++ main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	fmt.Println("Hello, world!")
 	a := app.New()
+	defer a.Quit()
 	w := a.NewWindow("Hello World")
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter text...")
+	content := container.NewVBox(
+		input, widget.NewButton("click me", func() {
+			log.Println(input.Text)
+		}), widget.NewLabel("Hello World!"),
+	)
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	w.SetContent(content)
 	w.ShowAndRun()
+
 }
